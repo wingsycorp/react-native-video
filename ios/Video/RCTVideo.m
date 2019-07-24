8ff35d1308805207ed861164c37eb951afa7eacf
Jens Andersson <jens@fritan.com>
2019-07-24 17:18:14 +0100

    Try to start playback instantly

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -368,7 +368,8 @@ static int const RCTVideoUnset = -1;
         
       _player = [AVPlayer playerWithPlayerItem:_playerItem];
       _player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
-        
+      _player.automaticallyWaitsToMinimizeStalling = false;
+      
       [_player addObserver:self forKeyPath:playbackRate options:0 context:nil];
       _playbackRateObserverRegistered = YES;
       
